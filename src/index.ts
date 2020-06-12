0954f81c9eadc6ad3d8219612815ce4bf4d7cc02
danielHidalgo <hrdi3191@gmail.com>
2020-06-12 13:23:29 -0500

    github accion

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+  
+import * as core from '@actions/core';
+
+function run() {
+  const name: string = core.getInput('my_input');
+  if (name) {
+    core.debug(`Hello ${name}!`);
+    return core.setOutput('my_output', `Hello ${name}!`);
+  }
+  core.setFailed('my_input not specified!');
+}
+
+run();
